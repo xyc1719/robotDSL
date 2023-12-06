8310fef4d7cf9a23f75f25c61a79027bca7acd9f
xyc1719 <38127251+xyc1719@users.noreply.github.com>
2023-12-06 23:27:40 +0800

    Parser.py coding...

--- Parser.py
+++ Parser.py
@@ -1,3 +1,17 @@
+from logging import getLogger
+from ply.yacc import yacc
+from ConfigYamlLoader import MyConfigYamlLoader
+from Lexer import MyLexer
 
+logrecord = getLogger('Interpreter')
 class Parser:
-    ou
+    def __init__(self,configLoader: MyConfigYamlLoader,lexer:MyLexer):
+        self._lexer = lexer
+        self.tokens = lexer.tokens
+        self._yacc = yacc(module=self,debug=True)
+        self._configLoader=configLoader
+
+    def parseStr(self,str):
+        return self._yacc.parse(str,self._lexer.getLexer())
+
+    def
